f6e93a0b201ab0fbc15ad0452575a779d46018c0
jonbell <jonbell@spamcop.net>
2011-07-19 15:21:50 -0500

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bracket::Application.config.secret_token = '113c92642dfc5c9009dfe742a5d28c24105a004efc10e8e7f6eac1d6a60b2e5f9f4ba616fe99904f3c8820a29064e75d6168d526e6a25f73ec235ff48a9f440c'
